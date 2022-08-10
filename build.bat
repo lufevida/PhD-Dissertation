52a8cd06f058337b19f68bbe5e72c1f9195f590c
Luis Felipe Vieira Damiani <lufevida@gmail.com>
2022-08-09 18:34:12 -0700

    Update build script

---
+++ build.bat
@@ -0,0 +1,21 @@
+del dissertation.pdf
+
+latex -interaction nonstopmode dissertation.tex
+latex -interaction nonstopmode dissertation.tex
+bibtex thesis
+latex -interaction nonstopmode dissertation.tex
+latex -interaction nonstopmode dissertation.tex
+dvipdfm dissertation.dvi
+explorer.exe dissertation.pdf
+
+del dissertation.aux
+del dissertation.bbl
+del dissertation.blg
+del dissertation.dvi
+del dissertation.lof
+del dissertation.log
+del dissertation.lot
+del dissertation.mx1
+del dissertation.out
+del dissertation.toc
+del tex\*.aux
